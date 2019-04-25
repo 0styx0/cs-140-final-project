489f3f447c3aa49b43f804cff2e402523b06a1ff
David Meiseles <me@davidm.me>
2019-04-24 22:02:25 -0400

    did memory, p1 of project 1

--- project/Memory.java
+++ project/Memory.java
@@ -1,10 +1,15 @@
 package project;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 class Memory {
 
 	public static final int DATA_SIZE = 512;
 	private int[] data = new int[Memory.DATA_SIZE];
+	public final int CODE_SIZE = 256;
+	private final List<Instruction> code = new ArrayList<>();
+	private int changedDataIndex = -1;
 
 	int[] getData(int min, int max) {
 
@@ -20,11 +25,21 @@ class Memory {
 		return this.data[index];
 	}
 
+	int getChangedDataIndex() {
+		return this.changedDataIndex;
+	}
+
+	List<Instruction> getCode() {
+		return this.code;
+	}
+
 	void setData(int index, int value) {
 
 		if (index < 0 || index > Memory.DATA_SIZE - 1) {
 			throw new DataAccessException("Off limit");
 		}
+
+		this.changedDataIndex = index;
 
 		this.data[index] = value;
 	}
@@ -34,5 +49,52 @@ class Memory {
 		for (int i = 0; i < this.data.length; i++) {
 			this.data[i] = 0;
 		}
+
+		this.changedDataIndex = -1;
+	}
+
+	Instruction getCode(int index) {
+
+		if (0 <= index && index < code.size()) {
+			throw new CodeAccessException("Illegal access to code");
+		}
+
+		return this.code.get(index);
+	}
+
+	public Instruction[] getCode(int min, int max) {
+
+		if (!(0 <= min && min <= max && max < code.size())) {
+			throw new CodeAccessException("Illegal access to code");
+		}
+
+		Instruction[] temp = {};
+		temp = code.toArray(temp);
+		return Arrays.copyOfRange(temp, min, max);
+	}
+
+	void addCode(Instruction value) {
+
+		if (code.size() < CODE_SIZE) {
+			code.add(value);
+		}
+	}
+
+	void setCode(int index, Instruction instr) {
+
+		if (!(0 <= index && index < code.size())) {
+			throw new CodeAccessException("Illegal access to code");
+		}
+
+		code.set(index, instr);
+	}
+
+	void clearCode() {
+		code.clear();
+	}
+
+	int getProgramSize() {
+
+		return code.size();
 	}
 }
